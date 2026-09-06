bb16ba1955ad21d0412057933024962cb50674c8
Lucas Albers <lucas.b.albers@gmail.com>
2026-09-06 04:22:50 +0000

    fixup: restore assert-set brace indent (mimo F1 on #618)

--- tests/test_conversion_ledger.py
+++ tests/test_conversion_ledger.py
@@ -401,7 +401,7 @@ def test_ci_golden_regenerates_and_drift_checks_ledger():
         "mycelium_w1",
         "claude-code-plugins_w1",
         "dogfood_shell_w1",
-        }
+    }
     for w in pw.values():
         assert w["properties_asked"] >= w["properties_sat"]
         assert w["properties_sat"] >= w["sat_ground_truthed"]
